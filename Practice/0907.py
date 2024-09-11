c5c7aca1ba72e4899299a7c7d195493230dd831b
李沂凌 <maribel7777770@gmail.com>
2024-09-11 02:00:51 +0000

    modify

--- Practice/0907.py
+++ Practice/0907.py
@@ -1,9 +1,9 @@
 #預防crash&設定變數
 try: 
-    height = int(input("請輸入身高(公分):"))
+    height = float(input("請輸入身高(公分):"))
     if height > 300:
         raise Exception("輸入身高不可超過300")  #手動raise Exception
-    weight = int(input("請輸入體重(公斤):"))
+    weight = float(input("請輸入體重(公斤):"))
 except ValueError:       #except沒有限制幾個，但(多個)except&else只會執行其中一個
     print("輸入格式有誤，請重新輸入")
 except Exception as e:    #除了格式錯誤的其他所有錯誤都是exception(要在最後面，屬廣泛錯誤)
@@ -16,7 +16,7 @@ else:
 #體重分析    
     if BMI < 18.5:          
         print ("您的體重過輕")
-    elif BMI <= 24:
+    elif BMI >= 18.5 and BMI < 24:       #跟elif BMI < 24: 有什麼不一樣
         print ("您的體重在正常範圍")
     elif BMI <= 27:
         print ("您的體重過重")
